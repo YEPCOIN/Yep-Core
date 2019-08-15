5b7d5f35af232df12fbc209ef55092275bfb9c70
YEPCOIN <53779100+YEPCOIN@users.noreply.github.com>
2019-08-15 23:18:43 +0200

    missing files

---
+++ src/invalid_serials.json.h
@@ -0,0 +1,18 @@
+// Copyright (c) 2018 The yep developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+
+
+#ifndef yep_INVALID_SERIALS_JSON_H
+#define yep_INVALID_SERIALS_JSON_H
+
+#include <string>
+
+std::string LoadInvalidSerials()
+{
+    std::string str ="[]";
+    return str;
+}
+
+#endif //yep_INVALID_SERIALS_JSON_H
